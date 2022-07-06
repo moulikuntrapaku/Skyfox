ce5addb38e0f9a824d712aa08e10ac1097c77c17
Shabnam <shabnam.tkthoughtworks.com@Shabnams-MacBook-Pro.local>
2022-07-06 21:09:37 +0530

    [shabnam|kabita]  resolve merge conflict

--- src/components/Profile/hooks/useChangePassword.js
+++ src/components/Profile/hooks/useChangePassword.js
@@ -71,4 +71,4 @@ export default (handleAuthChangePassword) => {
         errorMessage: errorMessage,
         handleChangePassword: handleChangePassword
     };
-};
+}; 
---
+++ src/components/Profile/toastMessage.js
@@ -0,0 +1,25 @@
+// import {toast}  from "react-toastify";
+// import 'react-toastify/dist/ReactToastify.css'
+// const defaultPosition = toast.POSITION.BOTTOM_LEFT;
+
+// export const showToast = ( type = "success", msg, autoClose = 2000, className = "primaryColor", position = defaultPosition ) => {
+//   if (type === "success") {
+//     toast.success(msg, {
+//       autoClose: autoClose === null ? 2000 : autoClose,
+//       className: className === null ? "primaryColor" : className,
+//       position: position,
+//     });
+//   } else if (type === "error") {
+//     toast.error(msg, {
+//       autoClose: autoClose === null ? 2000 : autoClose,
+//       className: className === null ? "dangerColor" : className,
+//       position: position,
+//     });
+//   }
+// };
+
+
+// // How to use react-toastify
+// showToast('success', 'Product add to cart successfully !');
+
+// showToast('error', 'Please fill the inputs properly.');
