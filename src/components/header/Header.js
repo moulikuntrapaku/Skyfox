8d55d1d6f60d3f7db5d91ab1728b14494a024173
Lasya Appali <lasya.appali@thoughtworks.com>
2022-07-13 20:44:41 +0530

    [Lasya] add. implement functionality to show username beside welcome in header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,78 +5,86 @@ import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import ProfileIcon from '@material-ui/icons/Person';
 import styles from "./styles/headerStyles";
 import PropTypes from "prop-types";
-import useProfile from "../Profile/hooks/useProfile";
-
+import profile from "../Profile/hooks/useProfile";
+import { getUsername } from "../../helpers/authService";
+ 
 const Header = ({ onLogout, isAuthenticated}) => {
-    const classes = styles();
-
-    const logoutSection = () => {
-        if (isAuthenticated) {
-            return (
-                <div id ="logoutSection" onClick={onLogout} className={classes.profilediv}>
-                    <ExitToAppIcon/>
-                    <Typography className={classes.headerLogo} variant="body1">
-                        Logout
-                    </Typography>
-                </div>
-            );
-        }
-    };
-
-    const profileSection = () => {
-        if (isAuthenticated) {
-            return (
-                <div id="profileSection" className={classes.logoutLink}>
-
+   const classes = styles();
+   const username=getUsername();
+   const logoutSection = () => {
+       if (isAuthenticated) {
+           return (
+               <div id ="logoutSection" onClick={onLogout} className={classes.profilediv}>
+                   <ExitToAppIcon/>
                    <Typography className={classes.headerLogo} variant="body1">
-                    <div className={classes.profilediv}>
-                      Welcome 
-                      <a href="/Profile"> <ProfileIcon style={{color:"white"}}/></a>
-                      </div>
-                    </Typography> 
-                </div>
-            );
-        }
-    };
-  
-
-
-    return (
-        <AppBar position={"sticky"}>
-            <Toolbar className={classes.toolbar}>
-                <a href="/" className={classes.headerLink}>
-                   <MovieIcon className={classes.cinemaLogoIcon}/>
-                   &nbsp;
-                   &nbsp;
-                    <Typography className={classes.headerLogo} variant="h5">
-                        SkyFox Cinema
-                    </Typography>
-                    <div className={classes.commonDiv}>
-                        <logoutSection className={classes.logoutLink}/>
-                        <profileSection className={classes.logoutLink}/>
-                    </div>
-                        
-                </a>
-                <div className={classes.headerActions}>
-                 {profileSection()} &nbsp; &nbsp;
-                 {logoutSection()}
-                </div>
-                
-            </Toolbar>
-        </AppBar>
-    );
+                       Logout
+                   </Typography>
+               </div>
+           );
+       }
+   };
+ 
+   const profileSection = () => {
+       if (isAuthenticated) {
+           return (
+               <div id="profileSection" className={classes.logoutLink}>
+ 
+                  <Typography className={classes.headerLogo} variant="body1">
+                   <div className={classes.profilediv}>
+                   {profile.Role === "ADMIN" ? (
+                        <div>Welcome Admin</div>
+                     ):
+                     (
+                       <div>Welcome {username}</div>
+                     )
+                   }
+                     <a href="/Profile"> <ProfileIcon style={{color:"white"}}/></a>
+                     </div>
+                   </Typography>
+               </div>
+           );
+       }
+   };
+ 
+ 
+   return (
+       <AppBar position={"sticky"}>
+           <Toolbar className={classes.toolbar}>
+               <a href="/" className={classes.headerLink}>
+                  <MovieIcon className={classes.cinemaLogoIcon}/>
+                  &nbsp;
+                  &nbsp;
+                   <Typography className={classes.headerLogo} variant="h5">
+                       SkyFox Cinema
+                   </Typography>
+                   <div className={classes.commonDiv}>
+                       <logoutSection className={classes.logoutLink}/>
+                       <profileSection className={classes.logoutLink}/>
+                   </div>
+                      
+               </a>
+               <div className={classes.headerActions}>
+                {profileSection()} &nbsp; &nbsp;
+                {logoutSection()}
+               </div>
+              
+           </Toolbar>
+       </AppBar>
+   );
 };
-
+ 
 Header.propTypes = {
-    onLogout: PropTypes.func.isRequired,
-    isAuthenticated: PropTypes.bool.isRequired
+   onLogout: PropTypes.func.isRequired,
+   isAuthenticated: PropTypes.bool.isRequired
 };
-
+ 
 export default Header;
-
-
-
-
-
-
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
 
